2dbd56efa986869c8cc85aaae5da79b6209c2cff
Bárbara Morán <barbaramoranrr@gmail.com>
2023-08-03 11:23:24 +0200

    add files and components structure and routing

---
+++ src/app/components/q-and-a/q-and-a.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { QAndAComponent } from './q-and-a.component';
+
+
+
+@NgModule({
+  declarations: [
+    QAndAComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    QAndAComponent
+  ]
+})
+export class QAndAModule { }
